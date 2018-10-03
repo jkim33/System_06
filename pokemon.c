d43d91c0a5fad90eb06576fa19dc5083a0108211
jkim33 <jkim33@stuy.edu>
2018-10-03 11:30:05 -0400

    work

--- pokemon.c
+++ pokemon.c
@@ -1,11 +1,21 @@
+//Richard Wong & Jason Kim
+//System Level Programming -- Pd.5
+//Work#06 -- Con-struct-ion
+//10-3-18
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 struct dex {int num; char pokemon[64];};
+char* poke[] = {"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise"};
 
 struct dex example() {
   struct dex ex;
+  int a = rand()%9;
+  strcpy(ex.pokemon, poke[a]);
+  ex.num = (a+1);
   return ex;
 }
 
@@ -18,19 +28,24 @@ int print(struct dex poke) {
 int modify(struct dex *poke, int n, char *p){
   poke->num = n;
   strcpy(poke->pokemon, p);
-  //poke->pokemon = p;
+  return 0;
 }
 
 int main() {
-  struct dex bulb;
-  bulb = example();
-  modify(&bulb, 1, "Bulbasaur");
+  srand(time(NULL));
   
-  struct dex ivy;
-  ivy = example();
-  modify(&ivy, 2, "Ivysaur");
+  struct dex poke1;
+  poke1 = example();
   
-  print(bulb);
-  print(ivy);
+  struct dex poke2;
+  poke2 = example();
+
+  struct dex overrated;
+  overrated = example();
+  modify(&overrated, 6, "Charizard");
+  
+  print(poke1);
+  print(poke2);
+  print(overrated);
   return 0;
 }
